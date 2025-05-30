28c38bb13f56fa42786bf04886a1ae7964c727d1
edgaras <edgaras.malukas@gmail.com>
2025-05-30 13:23:30 +0300

    Phpstan, php-cs-fixer and phpunit added to the project, project configured.

---
+++ .php-cs-fixer.php
@@ -0,0 +1,21 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(['src', 'tests'])
+    ->exclude('vendor')
+    ->name('*.php');
+
+$config = new PhpCsFixer\Config();
+$config->setRules([
+    '@PSR12' => true,
+    '@PhpCsFixer' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'no_trailing_whitespace' => true,
+
+    'general_phpdoc_annotation_remove' => [
+        'annotations' => ['internal', 'coversNothing'],
+    ],
+])
+    ->setFinder($finder);
+
+return $config;
