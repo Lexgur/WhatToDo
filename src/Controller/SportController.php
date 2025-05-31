4728535ee204a18aa4665af1194332811354fe3d
edgaras <edgaras.malukas@gmail.com>
2025-05-31 10:57:53 +0300

    Sport model added, missing getters and setters

--- src/Controller/SportController.php
+++ src/Controller/SportController.php
@@ -5,12 +5,22 @@ declare(strict_types=1);
 namespace Edgaras\WhatToDo\Controller;
 
 use Edgaras\WhatToDo\Attribute\Path;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 #[Path('/sportas')]
-class SportController
+class SportController extends AbstractController
 {
-    public function __invoke()
+    private SportRepository $repository;
+
+    public function __construct(SportRepository $repository)
     {
-        // TODO: Implement __invoke() method.
+        $this->repository = $repository;
+    }
+
+    public function __invoke(): JsonResponse
+    {
+        $data = $this->repository->getSportData();
+
+        return new JsonResponse($data);
     }
 }
